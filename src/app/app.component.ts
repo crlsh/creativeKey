01419e22ae56466ec63dc5e1873a37960a19bae7
crlsh <66803953+crlsh@users.noreply.github.com>
2023-09-10 20:01:32 -0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+
+ options= [
+    "Opción 1",
+    "Opción 2",
+    "Opción 3",
+    "Opción 4",
+    "Opción 5"
+  ]
+  selectedOption: string = '';
+  spinning: boolean = false;
+  
+
   title = 'creativeKey';
+
+
+  spinWheel() {
+    if (this.options.length > 0) {
+      this.spinning = true;
+      setTimeout(() => {
+        const randomIndex = Math.floor(Math.random() * this.options.length);
+        this.selectedOption = this.options[randomIndex];
+        this.spinning = false;
+      }, 2000); // Cambia la duración según tus preferencias
+    }
+  }
 }
+
